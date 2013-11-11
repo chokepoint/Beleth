ee630c414f7b7492b3669055cb329c04f8027a57
clutton <clutton@zoho.com>
2013-11-11 05:09:30 +0200

    function variables identification style changing

--- ssh.c
+++ ssh.c
@@ -19,15 +19,15 @@
  * Used while dropping the payload and waiting for the response.
  */
 int waitsocket(int socket_fd, LIBSSH2_SESSION *session) {
-    struct timeval timeout;
     int rc;
+    int dir;
     fd_set fd;
     fd_set *writefd = NULL;
     fd_set *readfd = NULL;
-    int dir;
- 
-    timeout.tv_sec = 10;
-    timeout.tv_usec = 0;
+    struct timeval timeout = {
+	    .tv_sec = 10, /* 10 seconds at most */
+	    .tv_usec = 0  /* nanoseconds */
+    };
  
     FD_ZERO(&fd);
  
